ba55fcf588ad120824e299a5a6755c6afce8906f
kaio ksianskievis <ksianskieviskaio@gmail.com>
2024-07-02 19:23:53 -0300

    Atualizando model User, adicionando o spring Security e fazendo algumas configurações na pasta config

---
+++ src/main/java/com/kaioKsianskievis/TodoList_Authenticated/Config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.kaioKsianskievis.TodoList_Authenticated.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    //classe de configurações
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception{
+        httpSecurity.csrf((csrf) -> csrf.disable())
+            .sessionManagement((session) ->session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests((auth) ->auth
+                .anyRequest().authenticated());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager(); //fazer verificações no banco
+    }
+
+    //criptografar a senha no banco de dados
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+
+}
--- src/main/java/com/kaioKsianskievis/TodoList_Authenticated/Models/User.java
+++ src/main/java/com/kaioKsianskievis/TodoList_Authenticated/Models/User.java
@@ -1,7 +1,11 @@
 package com.kaioKsianskievis.TodoList_Authenticated.Models;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -21,7 +25,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
     
     //atributos
 
@@ -43,4 +47,33 @@ public class User {
     @OneToMany(mappedBy = "userId")
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private List<Tasks> task = new ArrayList<Tasks>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        
+        return true;
+    }
 }
